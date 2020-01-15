4fcd60edd2f765758fc3cbc1db9f3f4c81fd52fd
Samiylo <skryshu@gmail.com>
2020-01-15 17:23:40 -0600

    Excercies 4

--- homwork2.py
+++ homwork2.py
@@ -30,16 +30,25 @@
 #    Im assuming that I already formatted the funtion like that, so its gonna be the same.
 #   
 
-a = [[1,3,4],[2,4,4]]
-b = [[5,2,2], [1,0,3]]
-c = []
+# a = [[1,3,4],[2,4,4]]
+# b = [[5,2,2], [1,0,3]]
+# c = []
 
-for index1 in range(len(a)):
-    small_list = []
-    for number in range(len(a[index1])):
-        new_number = a[index1][number] + b[index1][number]
-        small_list.append(new_number)
-    c.append(small_list)
-print(c)
+# for index1 in range(len(a)):
+#     small_list = []
+#     for number in range(len(a[index1])):
+#         new_number = a[index1][number] + b[index1][number]
+#         small_list.append(new_number)
+#     c.append(small_list)
+# print(c)
+
+
+# Excercise 4
+
+numbers = [1, 2, 2, 5, 3 ,4 ,5 ]
+print(numbers)
+new_numbers = set(numbers)
+
+print(new_numbers)
 
 
