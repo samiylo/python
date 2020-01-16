562b71c7177dc37f4d303a76f19596a34198e070
Samiylo <skryshu@gmail.com>
2020-01-16 12:05:34 -0600

    Update

---
+++ homework3.py
@@ -0,0 +1,14 @@
+# Homework 3
+
+# Excercise 1
+
+
+def smallest(a):
+    b = copy(a)
+    set(b)
+    small_num = b[0]
+
+
+print(smallest([1, 2, 3, 4, 5, 6]))
+
+
--- homwork2.py
+++ homwork2.py
@@ -4,9 +4,12 @@
 
 # Excercise 1
 
-# lista = [1,2,3,4]
-# listb = [5,6,7,8]
-# c = [a * b for a, b in zip(lista, listb)]
+# num = [1, 2, 3, 4, 5]
+# num2 = [2, 3, 4, 5, 6]
+# num3 = []
+# for i in range (0, len(num)):
+#     num3.append(num[i] * num2[i])
+# print((num3))
 
 # print(c)
 
@@ -45,46 +48,79 @@
 
 # Excercise 4
 
-# numbers = [1, 2, 2, 5, 3 ,4 ,5 ]
-# print(numbers)
-# new_numbers = set(numbers)
-
-# print(new_numbers)
+# newlist=[]
+# for item in original:
+#     if item in newlist:
+#         print ("You don't need to add "+str(item)+" again.")
+#     else:
+#         newlist.append(item)
+#         print( "Added "+str(item))
+# print (newlist)
 
 # Excercise 5
 
-# phrase = input("I will translate.")
-# uphrase = phrase.upper()
+# normal_sentence = "I am a leet programmer".upper()
+# leet_list = list(normal_sentence)
+# for i in range(len(leet_list)):
+#     if leet_list[i] == 'A':
+#         leet_list[i] = '4'
+#     elif leet_list[i] == 'E':
+#         leet_list[i] = '3'
+#     elif leet_list[i] == 'G':
+#         leet_list[i] = '6'
+#     elif leet_list[i] == 'I':
+#         leet_list[i] = '1'
+#     elif leet_list[i] == 'O':
+#         leet_list[i] = '0'
+#     elif leet_list[i] == 'S':
+#         leet_list[i] = '5'
+#     elif leet_list[i] == 'T':
+#         leet_list[i] = '7'
+# leet_sentence = "".join(leet_list)
+# print(leet_sentence.lower())
 
-# leet_letters = {"A":4, "E":3, "G":6, "I":1, "O":0, "S":5, "T":7}
 
-# phrase_list = list(uphrase)
-# #print(phrase_list)
+# shakespeare = """All that glitters is not gold
+# Fair is foul, and foul is fair: Hover through the fog and filthy air.
+# Life ... is a tale Told by an idiot, full of sound and fury, Signifying nothing."""
+# shake_upper = shakespeare.upper()
+# shake_list = list(shake_upper)
+# leet_shake = []
+# leet = {"A" : "4", "E" : "3", "G" : "6", "I" : "1", "O" : "0", "S" : "5", "T" : "7"}
+# for letter in shake_upper:
+#     if letter in leet:
+#         letter = leet[letter]
+#         leet_shake.append(letter)
+#     else:
+#         leet_shake.append(letter)
+# final = "".join(leet_shake).lower()
+# print(final)
 
-# changed_letters = []
 
-# for letter in range(len(leet_letters)):
-#     if letter == phrase_list:
-#         changed_letters.append(leet_letters)
-
-# print(changed_letters)
-
-# Im lost here and cant seem to find a solution
 
 # Excercise 6
 
-word = "Hello"
-vowls = 'a', 'e', 'i', 'o', 'u'
-
-word_list = list(word)
-print(word_list)
-
-new_word = []
-print(new_word)
-for letter in word_list:
-    for index in vowls:
-        if letter == vowls:
-            new_word.append(vowls)
+# string_to_vowel_check = 'Good'
+# last_letter = ''
+# new_string = ''
+# for letter in string_to_vowel_check:
+#     is_vowel = False
+#     if letter == last_letter:
+#         if letter == 'a' or letter == 'A':
+#             is_vowel = True
+#         elif letter == 'e' or letter == 'E':
+#             is_vowel = True
+#         elif letter == 'i' or letter == 'I':
+#             is_vowel = True
+#         elif letter == 'o' or letter == 'O':
+#             is_vowel = True
+#         elif letter == 'u' or letter == 'U':
+#             is_vowel = True
+#     last_letter = letter
+#     if is_vowel == True:
+#         letter = letter*4
+#     new_string+= letter
+# print(new_string)
 
 # Excercise 7
 
