43cbf059ff3381f2a3d21163ba2b4c8992c74c74
Samiylo <skryshu@gmail.com>
2020-01-16 17:45:43 -0600

    Excercise 3

--- homework3.py
+++ homework3.py
@@ -3,13 +3,39 @@
 # Excercise 1
 
 
-def smallest(input):
+# def smallest(input):
     
-    sorted_list = sorted(input)
-    print(sorted_list)
-    print(sorted_list[0])
+#     sorted_list = sorted(input)
+#     print(sorted_list)
+#     print(sorted_list[0])
 
-    
-smallest([1, 5, 3, 2, 4])
+# smallest([1, 5, 3, 2, 4])
+
+# Excercise 2
+
+# def largest(input):
+
+#     large = 0
+#     sorted_list = sorted(input)
+#     for i in sorted_list:
+#         if i > large:
+#             large = i
+#     print(large)
+
+# largest([1,7,3,4,5,6])
+
+# Excercise 3
+
+strings =  ["hello", "my name", "your name"]
+
+def short(input):
+    shortest = ""
+
+    print(input)
+    return min(input,key=len)
+        
+    print(shortest)
 
 
+
+print(short(strings))
